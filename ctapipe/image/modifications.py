923f2472371fdb7fccc75b5a2069bfe643110331
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-05-12 18:17:26 +0200

    Start working on image modifications, namely noise and smearing

---
+++ ctapipe/image/modifications.py
@@ -0,0 +1,99 @@
+from abc import abstractmethod
+import numpy as np
+from ..core.component import TelescopeComponent
+
+# the most important part is the selection api!!
+
+
+def mask_broken(geom, pix_ids):
+    mask = np.ones(shape=geom.pix_id.shape, dtype=bool)
+    mask[pix_ids] = 0
+    return mask
+
+
+def mask_thresholds(image, upper, lower):
+    mask = np.ones(shape=geom.pix_id.shape, dtype=bool)
+    mask[image > upper] = 0
+    mask[image < lower] = 0
+    return mask
+
+
+def add_noise(
+    image,
+    noise_level,
+    rng=None,
+    mask=None,
+    upper_threshold=None,
+    lower_threshold=None,
+    correct_bias=True,
+):
+    # notes:
+    # mask and thresholds is probably not very useful
+    # mask is only useful if noise level is a scalar, could also provide the array directly
+    # maybe only noise level and bias, selection in tool?
+    if not rng:
+        rng = np.random.default_rng()
+    if mask is None:
+        mask = np.ones(shape=image.shape, dtype=int)
+    else:
+        mask = mask.astype(int)
+    if upper_threshold:
+        mask &= image < upper_threshold
+    if lower_threshold:
+        mask &= image > lower_threshold
+    mask *= noise_level
+    noisy_image = image.copy()
+    noisy_image += rng.poisson(mask)
+    if correct_bias:
+        noisy_image -= mask
+    return noisy_image
+
+
+def smear_image(image, geom, smear_factor):
+    # make clear what smear factor is supposed to mean and that only direct neighbors are taken into account
+    # smear factor can be an array as well -> selection in tool possible!
+    # a more sophisticated approach might make use of the pixel area, but thats complicated
+    if geom.pix_type.value == "hexagon":
+        max_neighbors = 6
+    elif geom.pix_type.value == "rectangular":  # thats probably labeld differently
+        max_neighbors = 8  # or 4? lookup neighbor matrix for a rect cam
+    else:
+        print(geom.pix_type)
+        raise Exception("Unknown pixel type")
+
+    diffused_image = (
+        (image * geom.neighbor_matrix).sum(axis=1) * smear_factor / max_neighbors
+    )
+    remaining_image = image * (1 - smear_factor)
+    smeared_image = remaining_image + diffused_image
+    return smeared_image
+
+
+class ImageModifier(TelescopeComponent):
+    """
+    ...
+    """
+
+    @abstractmethod
+    def __call__(self, tel_id: int, image: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Modifies a given image
+
+        Returns
+        -------
+        np.ndarray
+            modified image
+        """
+        pass
+
+
+class ImageSmearer(ImageModifier):
+    pass
+
+
+class NoiseAdder(ImageModifier):
+    pass
+
+
+class PixelValueSetter(ImageModifier):
+    pass
---
+++ ctapipe/image/tests/test_modifications.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy.testing import assert_allclose
+from ctapipe.instrument import CameraGeometry
+
+# perform tests with methods as well as components!
+
+
+def test_mask_broken():
+    assert False
+
+
+def test_mask_thresholds():
+    assert False
+
+
+def test_add_noise():
+    assert False
+
+
+def test_smear_image():
+    assert False
