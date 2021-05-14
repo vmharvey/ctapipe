ae3a5a62ea64b825784eed9ed43d1daed79d3aae
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-05-14 12:11:04 +0200

    Add unit tests, change API

--- ctapipe/image/modifications.py
+++ ctapipe/image/modifications.py
@@ -6,50 +6,55 @@ from ..core.component import TelescopeComponent
 
 
 def mask_broken(geom, pix_ids):
+    """
+    Create a boolean mask with deselected pixels
+    Does this really need a function?
+    """
     mask = np.ones(shape=geom.pix_id.shape, dtype=bool)
     mask[pix_ids] = 0
     return mask
 
 
-def mask_thresholds(image, upper, lower):
-    mask = np.ones(shape=geom.pix_id.shape, dtype=bool)
-    mask[image > upper] = 0
-    mask[image < lower] = 0
+def mask_thresholds(image, upper=None, lower=None):
+    """
+    Create a boolean mask with pixels selected based on thresholds.
+    Does this really need a function?
+    """
+    mask = np.ones_like(image, dtype=bool)
+    if upper:
+        mask[image > upper] = 0
+    if lower:
+        mask[image < lower] = 0
     return mask
 
 
-def add_noise(
-    image,
-    noise_level,
-    rng=None,
-    mask=None,
-    upper_threshold=None,
-    lower_threshold=None,
-    correct_bias=True,
-):
+def add_noise(image, noise_level, rng=None, correct_bias=True):
+    """
+    Create a new image with added poissonian noise
+    """
     # notes:
     # mask and thresholds is probably not very useful
     # mask is only useful if noise level is a scalar, could also provide the array directly
     # maybe only noise level and bias, selection in tool?
     if not rng:
         rng = np.random.default_rng()
-    if mask is None:
-        mask = np.ones(shape=image.shape, dtype=int)
-    else:
-        mask = mask.astype(int)
-    if upper_threshold:
-        mask &= image < upper_threshold
-    if lower_threshold:
-        mask &= image > lower_threshold
-    mask *= noise_level
     noisy_image = image.copy()
-    noisy_image += rng.poisson(mask)
+    noise = rng.poisson(noise_level)
+    noisy_image += noise
     if correct_bias:
-        noisy_image -= mask
+        # which one to use?
+        # Fluctuations that have no bias per pixel
+        noisy_image -= noise_level
+        # No bias over the whole image
+        # noisy_image -= np.mean(noise)
     return noisy_image
 
 
 def smear_image(image, geom, smear_factor):
+    """
+    Create a new image with values smeared to the direct pixel neighbors
+    Pixels at the camera edge lose charge this way.
+    """
     # make clear what smear factor is supposed to mean and that only direct neighbors are taken into account
     # smear factor can be an array as well -> selection in tool possible!
     # a more sophisticated approach might make use of the pixel area, but thats complicated
--- ctapipe/image/tests/test_modifications.py
+++ ctapipe/image/tests/test_modifications.py
@@ -1,21 +1,65 @@
 import numpy as np
 from numpy.testing import assert_allclose
 from ctapipe.instrument import CameraGeometry
+from ctapipe.image import modifications
 
 # perform tests with methods as well as components!
 
 
 def test_mask_broken():
-    assert False
+    geom = CameraGeometry.from_name("LSTCam")
+    broken_pixel_ids = [1, 5, 10]
+
+    expected = np.ones_like(geom.pix_id, dtype=bool)
+    expected[broken_pixel_ids] = 0
+    mask = modifications.mask_broken(geom, broken_pixel_ids)
+
+    assert (mask == expected).all()
 
 
 def test_mask_thresholds():
-    assert False
+    image = np.array([1, 2, 3, 4, 5])
+    mask = modifications.mask_thresholds(image, 4.5, 2.1)
+    expected = [False, False, True, True, False]
+
+    assert (mask == expected).all()
 
 
 def test_add_noise():
-    assert False
+    image = np.array([0, 0, 5, 1, 0, 0])
+    rng = np.random.default_rng(42)
+    # test different noise per pixel:
+    noise = [6, 8, 0, 7, 9, 12]
+    noisy = modifications.add_noise(image, noise, rng, correct_bias=False)
+    assert image[2] == noisy[2]
+    # For other seeds there exists a probability > 0 for no noise added at all
+    assert noisy.sum() > image.sum()
+
+    # test scalar
+    noisy = modifications.add_noise(image, 20, rng, correct_bias=False)
+    diff_no_bias = noisy - image
+    assert (noisy > image).all()
+
+    # test bias
+    noisy = modifications.add_noise(image, 20, rng, correct_bias=True)
+    assert np.sum(diff_no_bias) > np.sum(noisy - image)
 
 
 def test_smear_image():
-    assert False
+    geom = CameraGeometry.from_name("LSTCam")
+    image = np.zeros_like(geom.pix_id, dtype=np.float64)
+    # select two pixels, one at the edge with only 5 neighbors
+    # Thats why we divide by 6 below
+    signal_pixels = [1, 1853]
+    neighbors = geom.neighbor_matrix[signal_pixels]
+    for signal_value in [1, 5]:
+        image[signal_pixels] = signal_value
+        for s in [0, 0.2, 1]:
+            smeared_light = s * signal_value
+            smeared = modifications.smear_image(image, geom, s)
+            assert np.isclose((image.sum() - smeared.sum() - smeared_light / 6), 0)
+            neighbors_1 = smeared[neighbors[0]]
+            neighbors_1853 = smeared[neighbors[1]]
+            assert_allclose(neighbors_1, smeared_light / 6)
+            assert_allclose(neighbors_1853, smeared_light / 6)
+            assert_allclose(smeared[signal_pixels], signal_value - smeared_light)
